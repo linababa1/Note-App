b228c1567f47f04eb53e08a864ba4e95db804db5
linababa1 <linababa987@gmail.com>
2023-03-18 17:34:34 +0200

    Note app

---
+++ src/dummy_notes.js
@@ -0,0 +1,10 @@
+const notes = [
+    {id: 1, title: 'i love to code.',
+    details: 'i want to code till death.', date: '3 march 2023',
+    },{id: 1, title: 'i love to code.',
+    details: 'i want to code till death.', date: '3 march 2023',
+    }
+
+]
+
+export default notes;
